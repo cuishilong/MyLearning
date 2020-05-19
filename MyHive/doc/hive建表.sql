acf8135a3a0d46ead9e189d25058797d8fb0ce65
arica <15238088486@qq.com>
2020-05-19 09:41:37 +0800

    hive建表模板

---
+++ MyHive/doc/hive建表.sql
@@ -0,0 +1,16 @@
+-- hive建表模板
+
+-- drop table if exists tmp.tmp_tmp ;
+create external table tmp.tmp_tmp
+(
+    id   int comment "id",
+    name string comment "name",
+    note string comment "注释"
+) comment "构建分区表"
+    partitioned by (dt string,status string)
+    clustered by (id) into 5 buckets
+    row format delimited fields terminated by '|'
+    stored as textfile
+    -- row format delimited fields terminated by '|' stored as parquet
+    -- row format delimited fields terminated by '|' stored as sequencefile
+    location '/user/hadoop/warehouse/log_test';
