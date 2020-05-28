4a9cb254fdc6ff0d2ceb2282429f84fad4a3c1b7
arica <15238088486@qq.com>
2020-05-28 15:08:14 +0800

    cuishilong

--- MyHive/doc/hive语法.sql
+++ MyHive/doc/hive语法.sql
@@ -1,3 +1,10 @@
+-- 创建库
+
+create database if not exists tmp;
+
+show databases like 'tmp*';
+
+desc database tmp;
 -- hive建表模板
 
 -- drop table if exists tmp.tmp_tmp ;
@@ -9,10 +16,10 @@ create external table tmp.tmp_tmp
 ) comment "构建分区表"
     partitioned by (dt string,status string)
     clustered by (id) into 5 buckets
-    row format delimited fields terminated by '|'
-    stored as textfile
+    row format delimited fields terminated by '|' stored as textfile
     -- row format delimited fields terminated by '|' stored as parquet
     -- row format delimited fields terminated by '|' stored as sequencefile
     location '/user/hadoop/warehouse/log_test';
+
 
 
