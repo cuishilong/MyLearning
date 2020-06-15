d83af9d750604d63840926810359b930c9664e33
arica <15238088486@qq.com>
2020-06-15 14:43:46 +0800

    cuishilong

--- MyKafka/src/main/java/consumer/KafkaConsumerDemo.scala
+++ MyKafka/src/main/java/consumer/KafkaConsumerDemo.scala
@@ -8,25 +8,28 @@ import org.apache.kafka.clients.consumer.KafkaConsumer
 import scala.collection.JavaConversions._
 
 object KafkaConsumerDemo {
-  val kafkaProp = new Properties()
-  kafkaProp.setProperty("bootstrap.servers", "192.168.200.44:2181,192.168.200.44:2181,192.168.200.44:2181")
-  kafkaProp.setProperty("group.id", "test_group")
-  kafkaProp.setProperty("enable.auto.commit", "true")
-  kafkaProp.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-  kafkaProp.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  def main(args: Array[String]): Unit = {
+    val kafkaProp = new Properties()
+    kafkaProp.setProperty("bootstrap.servers", "192.168.200.44:2181,192.168.200.44:2181,192.168.200.44:2181")
+    kafkaProp.setProperty("group.id", "test_group")
+    kafkaProp.setProperty("enable.auto.commit", "true")
+    kafkaProp.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    kafkaProp.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
 
-  val consumer = new KafkaConsumer[String, String](kafkaProp)
+    val consumer = new KafkaConsumer[String, String](kafkaProp)
 
-  val topic_arr = List[String]("test")
-  consumer.subscribe(topic_arr)
+    val topic_arr = List[String]("test")
+    consumer.subscribe(topic_arr)
 
-  while (true) {
-    val record = consumer.poll(Duration.ofSeconds(1))
-    if (!record.isEmpty) {
-      record.iterator().foreach(e => {
-        println(s"consumer : ${e}")
-      })
+    while (true) {
+      val record = consumer.poll(Duration.ofSeconds(1))
+      if (!record.isEmpty) {
+        record.iterator().foreach(e => {
+          println(s"consumer : ${e}")
+        })
+      }
     }
   }
+
 }
 
